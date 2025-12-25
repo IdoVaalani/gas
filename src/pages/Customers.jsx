a2d75b8081edf5f31dac705c8d63145afc19e9ff
עידו ועלני <ido.vaalani@gmail.com>
2025-12-25 11:53:09 +0000

    File changes

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
 import { useNavigate } from "react-router-dom";
-import { createPageUrl } from "./utils";
+import { createPageUrl } from "../utils";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
