8040497aebba9ec6e8f4467978b3691ed8d36c91
עידו ועלני <ido.vaalani@gmail.com>
2026-03-02 08:19:17 +0000

    File changes

--- functions/roundItemPrices.ts
+++ functions/roundItemPrices.ts
@@ -13,7 +13,7 @@ Deno.serve(async (req) => {
         if (price !== null && price !== undefined) {
             const rounded = Math.round(price * 100) / 100;
             if (rounded !== price) {
-                await base44.asServiceRole.entities.פריט.update(item.id, { מחיר_מכירה: rounded });
+                await base44.asServiceRole.entities['\u05e4\u05e8\u05d9\u05d8'].update(item.id, { '\u05de\u05d7\u05d9\u05e8_\u05de\u05db\u05d9\u05e8\u05d4': rounded });
                 updated++;
             }
         }
