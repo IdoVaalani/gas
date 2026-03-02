6037e8e0620a4949dbcd15580895a9bd32d49016
עידו ועלני <ido.vaalani@gmail.com>
2026-03-02 10:49:02 +0000

    File changes

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -434,7 +434,7 @@ const sidebarMenuButtonVariants = cva(
   }
 )
 
-const SidebarMenuButton = React.forwardRef((
+const SidebarMenuButton = React.forwardRef(function SidebarMenuButtonInner(
   {
     asChild = false,
     isActive = false,
@@ -445,7 +445,7 @@ const SidebarMenuButton = React.forwardRef((
     ...props
   },
   ref
-) => {
+) {
   const Comp = asChild ? Slot : "button"
   const { isMobile, state } = useSidebar()
 
